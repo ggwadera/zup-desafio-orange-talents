ddba3c0a6bab6b5ddc862407666f01c8644fd8a3
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-01-04 15:38:22 -0300

    Initial commit

---
+++ src/test/java/talents/orange/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package talents.orange.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
