3a562c376b55ac59a4279256a3e21653303c3ebe
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-01-06 18:36:16 -0300

    remove exception handler inheritance

--- src/main/java/talents/orange/challenge/controller/exception/ControllerExceptionHandler.java
+++ src/main/java/talents/orange/challenge/controller/exception/ControllerExceptionHandler.java
@@ -5,11 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import talents.orange.challenge.service.exception.UniqueViolationException;
 
 @ControllerAdvice
-public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+public class ControllerExceptionHandler {
 
     private ResponseEntity<ApiError> handleResponse(ApiError err) {
         return ResponseEntity.status(err.getStatus()).body(err);
@@ -19,7 +18,7 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<ApiError> handleObjectNotFound(ObjectNotFoundException e) {
         return handleResponse(ApiError.builder()
             .status(HttpStatus.NOT_FOUND.value())
-            .message("Entity %s not found with identifier %s".formatted(e.getEntityName(), e.getIdentifier()))
+            .message("Recurso %s não encontrado com ID %s".formatted(e.getEntityName(), e.getIdentifier()))
             .build());
     }
 
